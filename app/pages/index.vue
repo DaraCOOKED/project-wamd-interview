3b598063aed8c1af1021c3f30b0b4d7fd3c8bf44
DaraCOOKED <dara.somnag020@gmail.com>
2026-08-06 14:17:04 +0700

    commit done

--- app/pages/index.vue
+++ app/pages/index.vue
@@ -6,11 +6,12 @@
   
    
   <FaqAccordion :faqs="[
-  { question: 'How long does each interview take?', answer: 'ot mean data te' },
-  { question: 'Can I use notes?', answer: 'bro som alok' },
-  { question: 'What happens if I don\'t pass on my first attempt?', answer: '...' },
-  { question: 'What should I bring?', answer: '...' },
-  { question: 'How soon will I hear back after my interview?', answer: '...' }
+  { question: 'How long does each interview take?', answer: 'Each interview takes approximately 3 hours.' },
+  { question: 'Can I use notes?', answer: 'Yes, you can use but not in the interview room' },
+  { question: 'What happens if I don\'t pass on my first attempt?', answer: 'You can choose another skill or maybe try again next year.' },
+  { question: 'What should I bring?', answer: 'You should bring your ID card and dress properly' },
+  { question: 'How soon will I hear back after my interview?', answer: 'Yes, you will hear back within a week after your interview.' },
+  {question:'If im not good in English can i join WMAD?', answer:'Yes, you can join WMAD even if you are not good in English. We have a supportive community and resources to help you improve your skills.'}
 ]" />
   
 </template>
